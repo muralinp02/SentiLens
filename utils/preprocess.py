02dd246232bdca05b348295ebfd085eeb0d266de
Murali N P <muralinp02@gmail.com>
2026-03-28 13:50:34 +0530

    Fix NLTK resource loading for deployment

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,12 +1,36 @@
+import os
 import re
 import string
+import nltk
 
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
+NLTK_DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), "nltk_data")
+
+if NLTK_DATA_DIR not in nltk.data.path:
+    nltk.data.path.append(NLTK_DATA_DIR)
+
+
+def ensure_nltk_resources():
+    resources = {
+        "corpora/stopwords": "stopwords",
+        "corpora/wordnet": "wordnet",
+        "corpora/omw-1.4": "omw-1.4",
+    }
+
+    for resource_path, resource_name in resources.items():
+        try:
+            nltk.data.find(resource_path)
+        except LookupError:
+            nltk.download(resource_name, download_dir=NLTK_DATA_DIR, quiet=True)
+
+
+ensure_nltk_resources()
 
 stop_words = set(stopwords.words("english"))
 lemmatizer = WordNetLemmatizer()
+
 
 def clean_text(text):
     if not isinstance(text, str):
